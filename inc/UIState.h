43235d549a05268bcd0bf39a1f298fe56cd4b6be
grant-tm <grant.t.morgan@gmail.com>
2024-06-05 23:37:33 -0700

    remove unused ui_state members

--- inc/UIState.h
+++ inc/UIState.h
@@ -5,37 +5,19 @@
 #include <string>
 #include <vector>
 
-#include "MKBDIO.h"
+#include "Database.h"
 #include "ThreadSafeQueue.h"
 #include "FileRecord.h"
 
 class UIState {
 public:
-    char frame = 's'; // s search, r results
-    
-    ThreadSafeQueue<char> *control_queue;
-
-    std::string search_buffer = "";
-    std::string search_query = "";
-    int search_cursor = 0;
-    bool search_exec = false;
-
+    sqlite3 *db = nullptr;
+    std::string search_query;
     std::vector<struct FileRecord> files;
-    int file_scroll = 0;
 
-    void process_inputs (void);
-    void input_dispatch (char input);
-    inline bool is_command (char input);
+public:
+    void search (const char *query);
 
-    void command_handler (char command);
-
-    void search_control_handler (char input);
-    void update_search_buffer (char input);
-    
-    // void result_control_handler(char c);
-
-    // int set_results_scroll(int scroll);
-    // int add_results_scroll(int offset);
 };
 
-#endif
+#endif //UISTATE_H
