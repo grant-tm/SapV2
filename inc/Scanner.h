4272111dcec13cdcd251e0b49312a06edad1441a
grant-tm <grant.t.morgan@gmail.com>
2024-06-05 23:36:52 -0700

    fix runaway thread spawning

--- inc/Scanner.h
+++ inc/Scanner.h
@@ -8,25 +8,23 @@
 #include <thread>
 #include <mutex>
 
-// External Inclusions
-#include "sqlite3.h"
-
 // Project Inclusions
 #include "Database.h"
 #include "SystemUtilities.h"
 #include "ThreadSafeQueue.h"
 #include "FileRecord.h"
-#include "DetectKey.h"
+#include "AudioFile.h"
+//#include "DetectKey.h"
 
 // Definitions
 namespace fs = std::filesystem;
 #define TRANSACTION_SIZE 2048
 
 // Delimiter check function
-bool inline char_is_delimiter (char);
+bool char_is_delimiter (char);
 
 // Character to lowercase conversion
-char inline to_lower (char);
+char to_lower (char);
 
 // Tag generation function
 std::vector<std::string> generate_auto_tags (const std::string &);
@@ -35,7 +33,7 @@ std::vector<std::string> generate_auto_tags (const std::string &);
 std::string concatenate_tags (const std::vector<std::string> &);
 
 // File processing function
-struct FileRecord *process_file (sqlite3 *, const fs::directory_entry &);
+struct FileRecord *process_file (const fs::directory_entry &);
 
 // File extension validation
 inline bool validate_file_extension (const fs::directory_entry *);
@@ -44,25 +42,25 @@ inline bool validate_file_extension (const fs::directory_entry *);
 std::vector<fs::path> find_sub_dirs (const fs::path &);
 
 // File processing requirement check
-bool requires_processing (sqlite3 *, const fs::directory_entry *);
+bool requires_processing (Database *, const fs::directory_entry *);
 
 // File queueing functions
-void queue_files (sqlite3 *, const fs::path &, 
+void queue_files (Database *, const fs::path &,
                 ThreadSafeQueue<fs::directory_entry> *);
 
-void queue_all_files (sqlite3 *, const fs::path &, 
+void queue_all_files (Database *, const fs::path &, 
                 ThreadSafeQueue<fs::directory_entry> *);
 
 // Processing queued files function
-void process_queued_files (sqlite3 *, 
+void process_queued_files (Database *,
         ThreadSafeQueue<fs::directory_entry> *,
         ThreadSafeQueue<struct FileRecord *> *);
 
 // Insert processed files function
-void insert_processed_files (sqlite3*, 
+void insert_processed_files (Database *,
     ThreadSafeQueue<struct FileRecord *> *);
 
 // Directory scanning function
-void scan_directory (sqlite3 *, const fs::path &);
+void scan_directory (Database *, const fs::path &);
 
 #endif // SCANNER_H
--- src/Scanner.cpp
+++ src/Scanner.cpp
@@ -1,4 +1,4 @@
-#include "..\inc\Scanner.h"
+#include "Scanner.h"
 
 // These delimiters are used to automatically generate tags from filenames
 bool inline char_is_delimiter (char ch) {
@@ -17,21 +17,58 @@ std::string to_string(const fs::path &p) {
     return std::string(u8str.begin(), u8str.end());
 }
 
+char *to_cstring(const fs::path &p) {
+    std::u8string u8str = p.u8string();
+    std::string str(u8str.begin(), u8str.end());
+
+    // Allocate memory for the C-style string
+    size_t str_size = str.size() + 1;
+    char *cstr = new char[str_size];
+
+    // Use strcpy_s to copy the string
+    strcpy_s(cstr, str_size, str.c_str());
+
+    return cstr;
+}
+
 // to_lower converts characters to their lowercase equivalent.
 // This function is used to convert tags to lowercase in generate_auto_tags()
-char inline to_lower (char c) {
+char to_lower (char c) {
     return static_cast<char>(std::tolower(static_cast<unsigned char>(c)));
 }
+
+// check if file extension is .mp3 or .wav
+bool validate_file_extension (const fs::directory_entry *file) {    
+    auto extension = to_string(file->path().extension());
+    if (extension == ".mp3" || extension == ".wav") {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool path_is_ascii(std::string path) {
+    for (char c : path) {
+        if (static_cast<unsigned char>(c) > 127) {
+            return false;
+        }
+    }
+    return true;
+}
+
+//=============================================================================
+// generating tags
+//=============================================================================
 
 // generate_auto_tags generates tags from filename based on a set of delimiters
 // the delimiters are set in char_is_delimiter()
 // tags must be 3 characters or greater
 // tags are returned in a vector of strings
-std::vector<std::string> generate_auto_tags(const std::string& file_name) {
-    
+std::vector<std::string> generate_auto_tags(const std::string &file_name) {
+
     std::vector<std::string> tags;
     std::string token;
-    
+
     // for all the characters in the filename:
     // nondelimiter characters are accumualted in 'token'
     // when a delimiter is hit, save token if long enough
@@ -44,7 +81,7 @@ std::vector<std::string> generate_auto_tags(const std::string& file_name) {
         }
         else if (!token.empty()) [[unlikely]] {
             // tags must be 3 or more characters
-            if(token.size() > 2) {
+            if (token.size() > 2) {
                 tags.push_back(token);
             }
             token.clear();
@@ -55,14 +92,14 @@ std::vector<std::string> generate_auto_tags(const std::string& file_name) {
     if (!token.empty() && token.size() > 2) {
         tags.push_back(token);
     }
-    
+
     return tags;
 }
 
 // concatenate strings in a vector of strings into one space delimited string
-std::string concatenate_tags(const std::vector<std::string>& tags) {
+std::string concatenate_tags(const std::vector<std::string> &tags) {
     std::string result;
-    for (const auto& tag : tags) {
+    for (const auto &tag : tags) {
         if (!result.empty()) [[likely]] {
             result += " ";
         }
@@ -71,90 +108,16 @@ std::string concatenate_tags(const std::vector<std::string>& tags) {
     return result;
 }
 
-// given a directory entry, find and record attributes in FileRecord struct
-struct FileRecord *process_file (sqlite3 *db, 
-                                    const fs::directory_entry& file) {
-    
-    // allocate memory for entry parameters
-    struct FileRecord *db_entry = new struct FileRecord;
+//=============================================================================
+// queue files
+//=============================================================================
 
-    // identification
-    db_entry->file_path = to_string(file.path());
-    db_entry->file_name = to_string(file.path().filename());
-    db_entry->file_size = static_cast<int>(fs::file_size(file.path()));
-    
-    // calculate file duration
-    db_entry->duration = 0;
-    
-    // default user tags
-    db_entry->num_user_tags = 0;
-    db_entry->user_tags = "";
-    
-    // generate auto tags
-    std::vector<std::string> tags = generate_auto_tags(db_entry->file_name);
-    db_entry->num_auto_tags = tags.size();
-    db_entry->auto_tags = concatenate_tags(tags);
-    
-    // default user bpm and key
-    db_entry->user_bpm = 0;
-    db_entry->user_key = 0;
-    
-    // TODO: predict bpm and key
-    db_entry->auto_bpm = 0;
-    std::string path = to_string(file.path());
-    db_entry->auto_key = 0/*kdet_detect_key(path)*/;
-
-    return db_entry;
-}
-
-// check if file extension is .mp3 or .wav
-inline bool validate_file_extension (const fs::directory_entry *file) {    
-    auto extension = to_string(file->path().extension());
-    if (extension == ".mp3" || extension == ".wav") {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-// Find the sub-directories in the top layer in a directory
-// 
-// This is used as a heuristic for dividing file scanning work between threads.
-// However, splitting work by sub-directory may not evenly divide work, and bare 
-// files in the root directory won't be scanned without changes to 
-// scan_directory() 
-std::vector<fs::path> find_sub_dirs (const fs::path& root_path) {
-    std::vector<fs::path> sub_dirs;
-    for (const auto& entry : fs::directory_iterator(root_path)) {
-        if (entry.is_directory()) {
-            sub_dirs.push_back(entry.path());
-        }
-    }
-    return sub_dirs;
-}
-
-bool path_is_ascii(std::string path) {
-    for (char c : path) {
-        if (static_cast<unsigned char>(c) > 127) {
-            return false;
-        }
-    }
-    return true;
-}
-
-// Check if file meets the requirements to be analyzed and included in the db
-// Files must exist, be a regular file, and have a .mp3 or .wav extension.
-bool requires_processing (sqlite3* db, const fs::directory_entry *file) {
-    if (file->is_regular_file() /*&& path_is_ascii(file->path().string()*/ && validate_file_extension(file) && 
-            !db_entry_exists(db, "audio_files", to_string(file->path())))[[unlikely]]{
-        return true;
-    
-    } else {
-        return false;
-    }
-}
-
-void queue_files(sqlite3 *db, const fs::path &dir_path, ThreadSafeQueue<fs::directory_entry> *proc_queue) {
+void queue_files
+(
+    Database *db, 
+    const fs::path &dir_path, 
+    ThreadSafeQueue<fs::directory_entry> *proc_queue
+) {
     std::vector<std::thread> threads;
     std::mutex thread_mtx;
     int max_threads = 12;
@@ -193,20 +156,34 @@ void queue_files(sqlite3 *db, const fs::path &dir_path, ThreadSafeQueue<fs::dire
     }
 }
 
-void queue_all_files (sqlite3 *db, const fs::path &dir_path, 
+// when the recursive scan is done, stop the process queue
+void queue_all_files (Database *db, const fs::path &dir_path,
                 ThreadSafeQueue<fs::directory_entry> *proc_queue ) {
     queue_files(db, dir_path, proc_queue);
     proc_queue->stop_producing();
 }
 
-void process_and_queue (sqlite3 *db, fs::directory_entry file, 
-    ThreadSafeQueue<struct FileRecord *> *insrt_queue) {
-    
-    struct FileRecord *procd_file = process_file(db, file);
-    insrt_queue->push(procd_file);
+//=============================================================================
+// process a file
+//=============================================================================
+
+// Check if file meets the requirements to be analyzed and included in the db
+// Files must exist, be a regular file, and have a .mp3 or .wav extension.
+bool requires_processing (Database *db, const fs::directory_entry *file) {
+    char *path = to_cstring(file->path());
+
+    if (file->is_regular_file() && validate_file_extension(file) &&
+        !db->entry_exists("audio_files", to_cstring(file->path().c_str()))) {
+        delete[] path;
+        return true;
+    }
+    else {
+        delete[] path;
+        return false;
+    }
 }
 
-void process_queued_files (sqlite3 *db, 
+void process_queued_files (Database *db,
         ThreadSafeQueue<fs::directory_entry> *proc_queue,
         ThreadSafeQueue<struct FileRecord *> *insrt_queue) {
 
@@ -216,7 +193,7 @@ void process_queued_files (sqlite3 *db,
         proc_queue->try_pop(file);
         
         if (!file.path().empty()) {
-            struct FileRecord *procd_file = process_file(db, file);
+            struct FileRecord *procd_file = process_file(file);
             insrt_queue->push(procd_file);
         }
     }
@@ -224,18 +201,67 @@ void process_queued_files (sqlite3 *db,
     insrt_queue->stop_producing();
 }
 
-void insert_processed_files (sqlite3* db, 
+// given a directory entry, find and record attributes in FileRecord struct
+struct FileRecord *process_file (const fs::directory_entry &file) {
+
+    std::string extension = to_string(file.path().extension());
+    if (extension == std::string(".wav")) {
+        WAV wav(file.path());
+        wav.parse();
+        wav.close();
+    }
+
+    // allocate memory for entry parameters
+    struct FileRecord *db_entry = new struct FileRecord;
+
+    // identification
+    db_entry->file_path = to_string(file.path());
+    db_entry->file_name = to_string(file.path().filename());
+    db_entry->file_size = static_cast<int>(fs::file_size(file.path()));
+
+    // calculate file duration
+    db_entry->duration = 0;
+
+    // default user tags
+    db_entry->num_user_tags = 0;
+    db_entry->user_tags = "";
+
+    // generate auto tags
+    std::vector<std::string> tags = generate_auto_tags(db_entry->file_name);
+    db_entry->num_auto_tags = tags.size();
+    db_entry->auto_tags = concatenate_tags(tags);
+
+    // default user bpm and key
+    db_entry->user_bpm = 0;
+    db_entry->user_key = 0;
+
+    // TODO: predict bpm and key
+    db_entry->auto_bpm = 0;
+    std::string path = to_string(file.path());
+    db_entry->auto_key = 0/*kdet_detect_key(path)*/;
+
+    return db_entry;
+}
+
+//=============================================================================
+// insert_processed_files
+//=============================================================================
+void insert_processed_files (Database *db, 
     ThreadSafeQueue<struct FileRecord *> *insrt_queue) {
     
     while (insrt_queue->is_producing()) {
         if(insrt_queue->size() >= TRANSACTION_SIZE) {
-            db_insert_files(db, insrt_queue);
+            db->insert_files(insrt_queue);
         }
     }
     while (!insrt_queue->empty()) {
-        db_insert_files(db, insrt_queue);
+        db->insert_files(insrt_queue);
     }
 }
+
+//=============================================================================
+// Scan
+//=============================================================================
 
 // scan_directory scans, processes, and inserts audio files into the database
 // Three threads are created and the following producer-consumer pipeline runs:
@@ -251,7 +277,12 @@ void insert_processed_files (sqlite3* db,
 //    insert_processed_files pops FileRecord objects off the insert queue and
 //    inserts their data as entries in the database. Insertions are broken up
 //    into transactions for faster insertion (see DBINT::db_insert_files)
-void scan_directory (sqlite3 *db, const fs::path& dir_path) {
+void
+scan_directory
+(
+    Database *db, 
+    const fs::path& dir_path
+) {
     
     ThreadSafeQueue<fs::directory_entry> proc_queue;
     proc_queue.start_producing();
@@ -260,14 +291,6 @@ void scan_directory (sqlite3 *db, const fs::path& dir_path) {
     insrt_queue.start_producing();
 
     std::vector<std::thread> threads;
-
-    //fprintf(stderr, "queueing files\n");
-    /*queue_all_files(db, dir_path, &proc_queue);
-    fprintf(stderr, "processing queued files\n");
-    process_queued_files(db, &proc_queue, &insrt_queue);
-    fprintf(stderr, "inserting files\n");
-    insert_processed_files(db, &insrt_queue);*/
-    
     threads.emplace_back(&queue_all_files, db, dir_path, &proc_queue);
     threads.emplace_back(&process_queued_files, db, &proc_queue, &insrt_queue);
     threads.emplace_back(&insert_processed_files, db, &insrt_queue);
